61486f570f7067c73a6ef3bebff1e71f395b5e44
SeanC2024 <seancwork2024@gmail.com>
2024-06-17 12:55:39 -0700

    bomn

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("SEANdis");
+let secretPassword = ("I love ye");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -13,14 +13,26 @@ button.addEventListener("click", function() {
 
     // 2. Create a compound conditional statement to check for the following:
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
-    
+    if (username === secretUsername && password === secretPassword){
+        message.innerHTML = "Good job pookie ;D";
+    }
     // CODE SOLO
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
     //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
     //  - Finally, if the password is incorrect AND the username is incorrect, update the text of the message div to say "Incorrect username and password. Hmm. Suspicious."
-    
-    
+    else if (username === secretUsername && password !== secretPassword){
+        message.innerHTML = "password wrong dumbass";
+    }
+    else if (username !== secretUsername && password === secretPassword){
+        message.innerHTML = "username wrong stupid";
+    }
+    else if (username !== secretUsername && password !== secretPassword){
+        message.innerHTML = "username and password wrong you should die";
+    }
+    else {
+        message.innerHTML = "Try aggain!!!!!!";
+    }
     // Bonus: If the username OR password fields are empty, update the text of the message div to say "You need to enter SOMETHING."
     //  - Hint: What string represents an "empty" message.
     
